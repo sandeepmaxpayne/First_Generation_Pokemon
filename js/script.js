dcdf0fddbb8851a563d6810c37bb3e18eee4546e
DESKTOP-D7NMFO9\Sandeep <www.sandeepbabu45gmail.com>
2021-03-02 10:01:23 +0530

    second commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 $(function(){
     var pokeUrl = "https://pokeapi.co/api/v2/generation/1";
+  //  var pokemonByNamae = "https://pokeapi.co/api/v2/pokemon/";
 
     $.getJSON(pokeUrl).done(function(data){
         console.log(data);
+        $("#success").append("Fetch pokemon success").css("color", "green");
+        $.each(data.pokemon_species, function(index, pokemon){
+            var name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+            var par = $("<p>").html("pokemon species no " + (index + 1) + " is " + name);
+            par.appendTo("#pokemon");
+        });
+    }).fail(function(){
+        console.log("Poke api failed to load");
+        $("#error").append("Cannot fetch pokemon !").css("color", "red").css("font-size", "25px");
+    }).always(function(){
+        console.log("Pokemon is awesome");
     });
 
 });
